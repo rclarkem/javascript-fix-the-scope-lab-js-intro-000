2ec7351b569828fc0e7c88ac28ab66cc30eea918
cheerful-animator-9488 <r.clarkemitchell@gmail.com>
2019-04-04 00:18:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var animal = 'dog'
 function myAnimal() {
   return animal
 }
-
 myAnimal()
 
 function yourAnimal() {
@@ -24,11 +23,11 @@ return n + two;
 
 
 var funkyFunction = function() {
-
+  return function() {
     return "FUNKY!"
-  
+  }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction()
