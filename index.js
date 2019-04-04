1fcc1a4d29a6aca325e7de328c9ffc88fea7830c
cheerful-animator-9488 <r.clarkemitchell@gmail.com>
2019-04-04 01:09:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var animal = 'dog'
+ animal = 'dog'
 
 function myAnimal(d) {
   return animal
@@ -6,9 +6,7 @@ function myAnimal(d) {
 myAnimal()
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+
   return animal
 }
 yourAnimal()
